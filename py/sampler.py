8bd1a1eb72130562822828e8e5238a479217eba1
Vedant Kumar <vk@vedantk.com>
2011-12-31 16:36:37 -0500

    Added wide band sampler.

--- py/sampler.py
+++ py/sampler.py
@@ -2,11 +2,15 @@
 
 import sys
 import wave
+import math
+import struct
+import random
 from markov import MarkovChain
 
 class Sampler:
     def __init__(self, fname, nr_secs=15):
         '''Load <nr_secs> of PCM data from <fname>.'''
+        self.initialize()
         self.fname = fname
         f = wave.open(self.fname, 'r')
         self.params = f.getparams()
@@ -14,6 +18,9 @@ class Sampler:
         frames = f.readframes(nr_frames)
         self.buf = [self.pcm_to_repr(frames[i:i+4])
                     for i in xrange(0, nr_frames * 4, 4)]
+
+    def initialize(self):
+        pass
 
     def pcm_to_repr(self, frame):
         '''Convert 4 bytes of PCM data into any format.'''
@@ -37,8 +44,20 @@ class Sampler:
                 print k / chunk, "%"
             out.writeframes(self.repr_to_pcm(next(gen)))
 
+class WideBandSampler(Sampler):
+    def initialize(self):
+        self.width = 16
+
+    def pcm_to_repr(self, frame):
+        val = struct.unpack('@i', frame)[0]
+        return int(val / self.width)
+
+    def repr_to_pcm(self, chunk):
+        val = self.width * chunk 
+        return struct.pack('@i', random.randint(0, self.width) + chunk)
+
 if __name__ == '__main__':
     print "Loading data..."
-    gen = Sampler(sys.argv[1])
+    gen = WideBandSampler(sys.argv[1])
     print "Sampling chain..."
     gen.sample('bsp-out.wav')
